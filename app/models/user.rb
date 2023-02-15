6e12029cd26b6d1e8db4485d95136e7b95686d84
Joseph <Jbonventre95@gmail.com>
2023-02-15 17:00:33 -0500

    Asterik to origin cors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
     has_many :stocks, through: :user_stocks
 
 
-    validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-    validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "only allows letters and numbers" } 
-    validates :password, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-    validates :balance, numericality: { greater_than_or_equal_to: 0, message: "must have a positive balance"}
+    # validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    # validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "only allows letters and numbers" } 
+    # validates :password, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    # validates :balance, numericality: { greater_than_or_equal_to: 0, message: "must have a positive balance"}
 
   
   
