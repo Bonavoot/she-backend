a30744c73c2bc8eb39fa7ef4d2af39033b13efbe
Joseph <Jbonventre95@gmail.com>
2023-02-17 15:11:44 -0500

    final

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create" 
   delete "/logout", to: "sessions#destroy"
 
+  post '/users/:id/watchlist_records', to: 'watchlist_records#create'
+
 end
